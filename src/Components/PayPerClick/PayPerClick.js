5ae3c0847d26dce3aa6ee4bda36842c0edfc670e
Esseson <Essesonahmed@yahoo.com>
2019-05-15 10:25:19 -0700

    adding paid adverts

--- src/Components/PayPerClick/PayPerClick.js
+++ src/Components/PayPerClick/PayPerClick.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import './PayPerClick.css';
 
 
 
 function payperclick(){
     return(
         <div>
-        <h2>Take a Look.</h2>
-        <hr>
-        </hr>
+                <h2>paid adverts</h2>
+
+                <hr>
+                </hr>
+    
+                <div class="d-flex justify-content-start pl-5">
+                   
+                    <img src="http://www.pausethemoment.com/wp-content/uploads/2016/12/New-Airbnb-Coupon-Code-37-OFF-Discount.jpg" alt="adverts" height="200" width="200" />
+                    <div class="d-flex justify-content-center pl-5">
+                    <img src="https://sg-everydayonsales-infomartglobal.netdna-ssl.com/wp-content/uploads/2017/04/Popeyes-Weekday-Deals.jpg" alt="adverts" height="200" width="200" />
+                    <div class="d-flex justify-content-end pl-5">
+                    <img src="https://discountcoupons.es/img/websites/amazon-promo.jpg" alt="adverts" height="200" width="200" />
+                    <div class="d-flex justify-content-end pl-5">
+                    <img src="https://www.tmonews.com/wp-content/uploads/2016/09/tmobileonefourthlinefree-660x467.png" alt="adverts" height="200" width="200" />
+                    <div class="d-flex justify-content-end pl-5">
+                    <img src="https://www.prlog.org/12080885-adidas-end-of-the-year-sale.jpg" alt="adverts" height="200" width="200" />
+                </div>
+                </div>
+                </div>
+                </div>
+                </div>
+
+                
+           
+
+
+
         </div>
 
 
